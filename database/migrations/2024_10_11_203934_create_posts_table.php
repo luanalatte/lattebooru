14c8ae744296abc933f36a0665983613902d3b40
Luana Latte <luana.latte.cat@gmail.com>
2024-10-18 20:34:56 -0300

    add user_id constraint

--- database/migrations/2024_10_11_203934_create_posts_table.php
+++ database/migrations/2024_10_11_203934_create_posts_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id');
+            $table->foreignId('user_id')->constrained('users');
             $table->char('md5', 32)->unique();
             $table->char('ext', 4);
             $table->string('filename');
