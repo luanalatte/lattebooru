df7434b086bd3b7aff428b57ac49b41f9818526d
Luana Latte <luana.latte.cat@gmail.com>
2025-01-26 11:59:37 -0300

    placeholder favicon

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -10,6 +10,8 @@
   <link rel="preconnect" href="https://rsms.me/">
   <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
 
+  <link rel="shortcut icon" href="https://api.iconify.design/mdi/image.svg" type="image/svg">
+
   @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
