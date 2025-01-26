a772f6c5ae79257b9cbc92168c7762c798f76f2f
Luana Latte <luana.latte.cat@gmail.com>
2025-01-26 15:29:55 -0300

    glob vite input

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import fg from 'fast-glob';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: fg.sync(['resources/css/**/*.css', 'resources/js/**/*.js']),
             refresh: true,
         }),
     ],
