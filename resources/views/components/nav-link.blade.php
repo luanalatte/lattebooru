2aa8617d7d38973a0880297f81b9ab6ed1b5c818
Luana Latte <luana.latte.cat@gmail.com>
2025-02-07 19:46:12 -0300

    fix

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,6 +1,6 @@
 @props(['icon', 'text', 'href'])
 
-<a class="link px-1 md:hidden" href={{ $href }}}}">
+<a class="link px-1 md:hidden" href={{ $href }}">
   <x-icon name="{{ $icon }}" /> {{ $text }}
 </a>
 <a class="border p-2 max-md:hidden" href={{ $href }}}}" title="{{ $text }}">
