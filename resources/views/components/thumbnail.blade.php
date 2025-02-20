ef7888d177dbc644ebb9e3fc1d326cb681542c0b
Luana Latte <luana.latte.cat@gmail.com>
2025-02-20 20:50:10 -0300

    fix

--- resources/views/components/thumbnail.blade.php
+++ resources/views/components/thumbnail.blade.php
@@ -3,7 +3,7 @@
 <a title="{{ $post->tags->pluck('name')->implode(' ') }}" href="{{ route('posts.show', [$post]) }}">
   <article @class(['relative aspect-square overflow-hidden bg-neutral-50 shadow-sm', 'border-2 border-blue-400' => $post->isAnimated])>
     <img @class(['h-full w-full object-contain']) loading="lazy" src="{{ $post->thumbnailUrl }}">
-    <div class="absolute bottom-0 z-20 w-full p-2 text-lg text-gray-300">
+    <div class="absolute bottom-0 z-1 w-full p-2 text-lg text-gray-300">
       @if ($post->is_private)
         <div title="{{ __("This post is :0.", [strtolower(__("Private"))]) }}">
           <x-icon name="mdi:lock" />
